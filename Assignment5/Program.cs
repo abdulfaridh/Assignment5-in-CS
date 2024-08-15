2ad65abb55c161b45d4289d0f2e6280bc00df969
farid <farid@LAPTOP-SPTVSQVM>
2024-08-15 09:11:51 +0530

    Add project files.

---
+++ Assignment5/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignment5
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter a:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter b:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("performing addition:");
+        add(a, b);
+        Console.WriteLine("performing subraction:");
+        sub(a, b);
+    }
+    public static void add(int x, int y)
+    {
+        Console.WriteLine($"{x} + {y} = {x + y}");
+    }
+
+    public static void sub(int x, int y)
+    {
+        Console.WriteLine($"{x} - {y} = {x-y}");
+    }
+}
